7384ab1f34f61ed8b99fe49957b2dae870c812f7
Daniel Felix <d.felix@codeinsider.net>
2013-09-14 21:52:13 +0200

    Update todo_item.rb
    Should do the same, but is better maintainable.
    
    Give you the possibility, to define some options, which could be used inside of to_json. The default output would be your previous hardcoded output.

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -4,19 +4,11 @@ class TodoItem < ActiveRecord::Base
   has_one    :issue, :primary_key => "issue_id", :foreign_key => "id",  :dependent => :delete
   acts_as_list
 
-  def as_json(options=nil)
-    {
-        :id => self.id,
-        :position => self.position,
-        :updated_at => self.updated_at,
-        # :name => self.issue.subject,
-        :issue_id => self.issue_id,
-        :todo_list_id => self.todo_list_id,
-        :subject => self.issue.subject,
-        :status_id => self.issue.status_id,
-        :assigned_to_id => self.issue.assigned_to_id,
-        :due_date => self.issue.due_date
-    }
+  def as_json(options = nil)
+    super (options || {
+        :include => :issue => {:only => [:subject, :status_id, :assigned_to_id, :due_date]},
+        :only => [:id, :position, :updated_at, :issue_id, :todo_list_id]
+    })
   end
 
 end
