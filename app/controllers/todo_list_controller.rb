b137d882e8df3310edb92f61725f3df0fc77850d
Adam Zielinski <kontakt@azielinski.info>
2013-09-15 23:25:53 +0200

    Fixed syntax error introduced in previous commit

--- app/controllers/todo_list_controller.rb
+++ app/controllers/todo_list_controller.rb
@@ -119,9 +119,11 @@ class TodoListController < ApplicationController
         }
     )
     comments_nbs = Hash.new
-    if comments_nbs_rs != -1 then comments_nbs_rs.each do |i|
-      values = i.kind_of?(Hash) ? i.values : i
-      comments_nbs[values[0]] = values[1]
+    if comments_nbs_rs != -1
+      comments_nbs_rs.each do |i|
+        values = i.kind_of?(Hash) ? i.values : i
+        comments_nbs[values[0]] = values[1]
+      end
     end
     comments_nbs
   end
