b2e31bed0f81a7c439f104a13f56f6bf4d63f5d2
Adam Zielinski <adam@adamziel.com>
2016-01-07 16:49:59 +0100

    Revert "Support for Redmine 2.6+, added tracker selection for todos"

--- db/migrate/003_add_default_tracker_to_project.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultTrackerToProject < ActiveRecord::Migration
-  def change
-    add_column :projects, :default_tracker, :string, :default => ""
-  end
-end
--- lib/redmine_todos/patches/issue_patch.rb
+++ lib/redmine_todos/patches/issue_patch.rb
@@ -14,4 +14,9 @@ module RedmineTodos
 
     end
   end
+end  
+
+
+unless Issue.included_modules.include?(RedmineTodos::Patches::IssuePatch)
+  Issue.send(:include, RedmineTodos::Patches::IssuePatch)
 end
